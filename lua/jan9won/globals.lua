a4c0cca22bd6b4c4ee598f58cc4eddea4c1c2ef0
Jangwon Suh <jan9won@gmail.com>
2025-09-10 08:57:19 +0900

    netrw config moved to init.nvim

--- lua/jan9won/globals.lua
+++ lua/jan9won/globals.lua
@@ -1,7 +1,3 @@
--- disable netrw at the very start of your init.lua
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-
 -- link folding (concealing)
 vim.opt.conceallevel = 0
 vim.opt.concealcursor = "nc"
